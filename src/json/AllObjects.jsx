189a144f3f1bc6e0e31933943764e4b31b2750c9
Breiner <leonardojgarciaparada2005@gmail.com>
2024-04-28 10:49:09 -0400

    Homepage Modified

---
+++ src/json/AllObjects.jsx
@@ -0,0 +1,83 @@
+import { MdCategory } from "react-icons/md";
+import { HiDocumentReport } from "react-icons/hi";
+import { ImExit } from "react-icons/im"; 
+import { FaArrowRightLong } from "react-icons/fa6";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { IoBulbSharp } from "react-icons/io5";
+import { FaHelmetSafety } from "react-icons/fa6";
+import { AiFillTool } from "react-icons/ai";
+import { GiSolderingIron } from "react-icons/gi";
+import { GiLargePaintBrush } from "react-icons/gi";
+import { GiNails } from "react-icons/gi";
+
+export const cards = [
+      {
+        id: 0,
+        title: "Electricidad",
+        color: "#FFD700",
+        icon: <IoBulbSharp size={70}/>,
+      },
+      {
+        id: 1,
+        title: "Plomería",
+        color: "#E65A24",
+        icon: <AiFillTool  size={70}/>,
+      },
+      {
+        id: 2,
+        title: "S.Laboral",
+        color: "#2F4F4F",
+        icon: <FaHelmetSafety size={70}/>,
+      },
+      {
+        id: 3,
+        title: "Metalurgia",
+        color: " #999999",
+        icon: <GiSolderingIron size={70} />,
+      },
+      {
+        id: 4,
+        title: "Pintura",
+        color: "#801817",
+        icon: <GiLargePaintBrush size={70}/>,
+      },
+      {
+        id: 5,
+        title: "Tornillería",
+        color: "#4E3D2E",
+        icon: <GiNails size={70}/>,
+      },
+    ];
+  
+  export const menuOption = [
+      {
+        id:0,
+        link: "/",
+        icon:<MdCategory className="position-absolute mx-2" style={{ top: "10px" }} size={30}/>,
+        title:"Ver Categorías"
+      },
+      {
+        id:1,
+        link: "/",
+        icon:<HiDocumentReport className="position-absolute mx-2" style={{ top: "10px" }} size={30}/>,
+        title:"Ver Reportes"
+      },
+      {
+        id:2,
+        link: "/",
+        icon:<FaArrowRightLong className="position-absolute mx-2" style={{ top: "10px" }} size={30}/>,
+        title:"Realizar Ingreso"
+      },
+      {
+        id:3,
+        link: "/",
+        icon:<FaArrowLeftLong className="position-absolute mx-2" style={{ top: "10px" }} size={30}/>,
+        title:"Realizar Salida"
+      },
+      {
+        id:4,
+        link: "/",
+        icon:<ImExit className="position-absolute mx-2" style={{ top: "10px" }} size={30}/>,
+        title:"Salir"
+      },
+    ]
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,126 +1,26 @@
 import { Link } from "react-router-dom";
-import { MdCategory } from "react-icons/md";
-import { HiDocumentReport } from "react-icons/hi";
-import { ImExit } from "react-icons/im";
-import { FaArrowRightLong } from "react-icons/fa6";
-import { FaArrowLeftLong } from "react-icons/fa6";
 import { GrDropbox } from "react-icons/gr";
-import { IoBulbSharp } from "react-icons/io5";
-import { FaHelmetSafety } from "react-icons/fa6";
-import { AiFillTool } from "react-icons/ai";
-import { GiSolderingIron } from "react-icons/gi";
-import { GiLargePaintBrush } from "react-icons/gi";
-import { GiNails } from "react-icons/gi";
+import { cards, menuOption } from "../json/AllObjects";
 function Homepage() {
-  const cards = [
-    {
-      id: 0,
-      title: "Electricidad",
-      color: "#FFD700",
-      icon: <IoBulbSharp size={70}/>,
-    },
-    {
-      id: 1,
-      title: "Plomería",
-      color: "#E65A24",
-      icon: <AiFillTool  size={70}/>,
-    },
-    {
-      id: 2,
-      title: "S.Laboral",
-      color: "#2F4F4F",
-      icon: <FaHelmetSafety size={70}/>,
-    },
-    {
-      id: 3,
-      title: "Metalurgia",
-      color: " #999999",
-      icon: <GiSolderingIron size={70} />,
-    },
-    {
-      id: 4,
-      title: "Pintura",
-      color: "#801817",
-      icon: <GiLargePaintBrush size={70}/>,
-    },
-    {
-      id: 5,
-      title: "Tornillería",
-      color: "#4E3D2E",
-      icon: <GiNails size={70}/>,
-    },
-  ];
   return (
     <>
       <div className="vh-100 d-flex">
         <div
-          className="shadow"
-          style={{ background: "#791021", width: "380px", height: "100%" }}
-        >
+          className="shadow menu">
           <h3 className="text-white text-center pt-5 mb-5">
             ¡Bienvenido Almacenista!
           </h3>
           <ul className="list-group list-group-flush">
+            {menuOption.map((opt) => (            
             <Link
-              to="/"
+              to={opt.link}
               className="position-relative list-group-item fw-bold mb-3 text-white"
-              style={{ background: "#F4D010" }}
+              style={{ background: "#F4D010", border: "0"}}
             >
-              <h4 className="d-inline">Ver Categorías</h4>
-              <MdCategory
-                className="position-absolute mx-2"
-                style={{ top: "10px" }}
-                size={30}
-              />
+              <h4 className="d-inline">{opt.title}</h4>
+            {opt.icon}
             </Link>
-            <Link
-              to="/"
-              className="position-relative list-group-item fw-bold mb-3 text-white"
-              style={{ background: "#F4D010" }}
-            >
-              <h4 className="d-inline">Ver Reportes</h4>
-              <HiDocumentReport
-                className="position-absolute mx-2"
-                style={{ top: "10px" }}
-                size={30}
-              />
-            </Link>
-            <Link
-              to="/"
-              className="position-relative list-group-item fw-bold mb-3 text-white"
-              style={{ background: "#F4D010" }}
-            >
-              <h4 className="d-inline">Realizar Ingreso</h4>
-              <FaArrowRightLong
-                className="position-absolute mx-2"
-                style={{ top: "10px" }}
-                size={30}
-              />
-            </Link>
-            <Link
-              to="/"
-              className="position-relative list-group-item fw-bold mb-3 text-white"
-              style={{ background: "#F4D010" }}
-            >
-              <h4 className="d-inline">Realizar Salida</h4>
-              <FaArrowLeftLong
-                className="position-absolute mx-2 "
-                style={{ top: "10px" }}
-                size={30}
-              />
-            </Link>
-            <Link
-              to="/"
-              className="position-relative list-group-item fw-bold text-white"
-              style={{ background: "#ff0000" }}
-            >
-              <h4 className="d-inline">Salir</h4>
-              <ImExit
-                className="position-absolute mx-2"
-                style={{ top: "12px" }}
-                size={30}
-              />
-            </Link>
+          ))}
           </ul>
           <div className="text-white text-center mt-5">
             <GrDropbox style={{ opacity: "0.5" }} size={200} />
@@ -140,7 +40,7 @@ function Homepage() {
                   style={{
                     background: card.color,
                   }}
-                  className="no-line text-white p-3 my-2 rounded-4 shadow"
+                  className="no-line text-white p-3 my-2 rounded-4 shadow card-scale"
                   type="button"
                 >
                   <div
